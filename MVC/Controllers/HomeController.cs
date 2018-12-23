aa98b97a3da7cf605a48b650e691a5b18b23f847
K <K@DESKTOP-96V19OA>
2018-12-23 15:40:17 +0800

    MVC 回忆

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -12,18 +12,35 @@ namespace MVC.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public void Index()
+        public ActionResult Index()
+        {
+            return View();
+            //PostMethodName();
+        }
+
+        public ActionResult Add(string i1, string i2)
+        {
+            return Content((int.Parse(i1) + int.Parse(i2)).ToString());
+        }
+
+
+
+
+        #region PostMethodName
+        private void PostMethodName()
         {
             string result = "";
             try
             {
-                Stream stream = HttpContext.Request.InputStream;
-                StreamReader reader = new StreamReader(stream);
-                string xmlString = reader.ReadToEnd();
-                var set = XmlHelper.GetDataSet(xmlString);
-                var dt = set.GetDataTable("DocumentElement");
-                string methord = dt.GetStringValue(0, "MethodName");
-                result = ReadFile(methord);
+                using (Stream stream = HttpContext.Request.InputStream)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string xmlString = reader.ReadToEnd();
+                    var set = XmlHelper.GetDataSet(xmlString);
+                    var dt = set.GetDataTable("DocumentElement");
+                    string methord = dt.GetStringValue(0, "MethodName");
+                    result = ReadFile(methord);
+                }
             }
             catch (Exception ex)
             {
@@ -37,5 +54,6 @@ namespace MVC.Controllers
             string path = string.Format(@"E:\0Project\0FaBu\Test3\1\{0}.txt", s);
             return FileHelper.ReadFile(path, Encoding.UTF8);
         }
+        #endregion
     }
 }
