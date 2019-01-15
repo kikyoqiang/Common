7d33df5473bd9e1a28553f8f4cd76112d94cfeb3
K <K@K-PC>
2019-01-15 11:30:06 +0800

    EnumClass

---
+++ Common/Extension/EnumClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public static class EnumClass
+    {
+        public enum DataBaseType
+        {
+            SqlServeType = 1,
+            OracleType = 2,
+            WebServiceType = 3,
+        }
+    }
+}
